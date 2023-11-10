954d3015011c06e9aaf0bec83b7163af8cc7a45e
Chris Butler <butlertree@gmail.com>
2023-11-10 12:01:03 -0700

    Added api fetch calls that are passed to a create traveler function

--- src/scripts.js
+++ src/scripts.js
@@ -7,11 +7,16 @@ import './css/styles.css';
 // import './images/turing-logo.png'
 
 //////////// Import fetch call from apiCalls.js //////////////
-// import { fetchAllTheData, allUsers, hydrationData, activityData, sleepData,  postHydrationData } from './apiCalls';
+import { 
+    fetchAllTheData,
+    allTravelers,
+    allTrips,
+    allDestinations
+} from './apiCall';
 
 //////////// Import functions from scriptDefinitions //////////////
 import {
-    showDashboard
+    addDataToCurrentTraveler
     } from './scriptDefinitions';
 
 // const {
@@ -36,17 +41,44 @@ import {
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault(); // Prevent the default form submission
   
+    // Get the values of the username and password fields
     const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
   
-    // Check the username and password (example: 'traveler' for both)
-    if (username === 'traveler' && password === 'traveler') {
-      // Successful login
-      loginSection.style.display = 'none'; // Hide login section
-      dashboardSection.style.display = 'block'; // Show dashboard section
+    // Split the username to separate the 'traveler' part and the number part
+    const usernameParts = username.split('traveler');
+  
+    // Check if there are two parts (i.e., 'traveler' + number)
+    if (usernameParts.length === 2) {
+      const travelerNumber = parseInt(usernameParts[1]);
+  
+      // Check if the number is greater than 0 and less than 51 and if the password is 'traveler'
+      if (travelerNumber > 0 && travelerNumber < 51 && password === 'traveler') {
+        // Successful login
+  
+        // Hide the login section and show the dashboard section
+        loginSection.style.display = 'none';
+        dashboardSection.style.display = 'block';
+  
+        // Fetch all the data and create the currentTraveler object
+        fetchAllTheData().then(() => {
+          const currentTravelerData = addDataToCurrentTraveler(
+            travelerNumber, // Pass the login ID
+            allTravelers, // Pass the variable directly
+            allTrips, // Pass the variable directly
+            allDestinations // Pass the variable directly
+          );
+  
+          // Use the currentTravelerData object as needed
+          console.log(currentTravelerData);
+        });
+      } else {
+        // Invalid login, show an error message or alert
+        alert('Invalid credentials. Please try again.');
+      }
     } else {
-      // Invalid login, show an error message or alert
-      alert('Invalid credentials. Please try again.');
+      // Invalid username format, show an error message or alert
+      alert('Invalid username format. Please use "traveler" followed by a number between 1 and 50.');
     }
   });
   
