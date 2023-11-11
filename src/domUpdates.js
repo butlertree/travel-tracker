36a792fab70243e330f5de263351c63d4a26facf
Chris Butler <butlertree@gmail.com>
2023-11-10 17:30:15 -0700

    Added functionality to update dom with pedning trip images

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -7,13 +7,35 @@ const loginForm = document.querySelector('#login-form');
 const travelerName = document.querySelector('#traveler')
 const loginSection = document.getElementById('login-section');
 const dashboardSection = document.getElementById('dashboard');
+const pendingTripsContainer = document.querySelector('#pending-trips')
 ///DOM UPDATES TO HERE
 
+
+//add user name to the dom 
 const updateUserName = (traveler) => {
     travelerName.innerHTML ='';
     travelerName.innerHTML += `<h1>${traveler.name}</h1></p>`;
   }
 
+  
+//add pending trip images to the dom
+  const updatePendingTrips = (pendingTripImageURLs) => {
+    // Clear the existing content in the container
+    pendingTripsContainer.innerHTML = '';
+  
+    // Loop through the pendingTripImageURLs and create thumbnail images
+    pendingTripImageURLs.forEach((imageUrl) => {
+      const tripThumbnail = document.createElement('img');
+      tripThumbnail.src = imageUrl; // Set the image URL
+      tripThumbnail.alt = 'Pending Trip Thumbnail'; // Set alt text for the image (you can customize this)
+  
+            // Add a CSS class to the image
+    tripThumbnail.classList.add('thumbnail-image');
+
+      // Append the thumbnail to the container
+      pendingTripsContainer.appendChild(tripThumbnail);
+    });
+  };
 
 
 ///EXPORTS GO HERE LIKE THIS TEMPLATE
@@ -24,5 +46,6 @@ export {
     loginSection,
     dashboardSection,
     loginForm,
-    updateUserName
+    updateUserName,
+    updatePendingTrips
   }
